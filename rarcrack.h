9fa6964040ad9454ac855d073c626e437373f50f
Justin Young <jayoungfl@gmail.com>
2011-08-16 16:12:54 -0400

    fixed a segfault during autodetect

--- rarcrack.h
+++ rarcrack.h
@@ -34,7 +34,7 @@ char default_ABC[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV
 const char CMD_DETECT[] = "file -i -b %s"; //this command return what is the file mime type
 
 const char* TYPE[] = {"rar",		"7z",		"zip", ""}; //the last "" signing this is end of the list
-const char* MIME[] = {"application/x-rar", "application/octet-stream", "application/x-zip", ""};
+const char* MIME[] = {"application/x-rar;", "application/x-7z-compressed;", "application/zip;", ""};
 const char* CMD[] = {"unrar t -y -p%s %s 2>&1", "7z t -y -p%s %s 2>&1", "unzip -P%s -t %s 2>&1", ""};
 
 #define PWD_LEN 100
